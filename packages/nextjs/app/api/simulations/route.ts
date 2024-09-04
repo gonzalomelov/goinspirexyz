fc490fa78d6d4fcf153078e7ef2bb5e3d2bdf61e
Gonzalo Melo <gonzalomelov@gmail.com>
2024-09-03 21:31:37 -0300

    chore: move group chat creation to the group-chat backend

--- packages/nextjs/app/api/simulations/route.ts
+++ packages/nextjs/app/api/simulations/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
-import { createGroupChat } from "../utils/xmtp";
+
+// import { createGroupChat } from "../utils/xmtp";
 
 // In-memory storage for simulations
 const simulations: {
@@ -22,21 +23,6 @@ export async function POST(request: Request) {
   const { target, targetFirstName, situation, privateInfo, groupTitle, groupImage } = await request.json();
 
   try {
-    // XMTP addresses
-    const creatorAddress = "0x372082138ea420eBe56078D73F0359D686A7E981";
-    const otherAddress = "0x5B385D961CDD40a54356b72B0A86f8A8dA2f2A62"; // iPhone 15 Pro Max
-    const targetAddress = "0x0571dd6bbaBb4Dded78858302664CE3407DF35e0"; // iPhone 15
-    const agentAddresses = [
-      "0x0D79E8F6A3F81420DDbFfaDAc4CD651335777a9D", // Mario: LEAD_AGENT_XMTP_ADDRESS
-      "0xeEE998Beb137A331bf47Aa5Fc366033906F1dB34", // Paul: TECH_AGENT_XMTP_ADDRESS
-      "0xE67b3617E9CbAf456977CA9d4b9beAb8944EFc37", // Emile: SOCIAL_AGENT_XMTP_ADDRESS
-      "0xfA568f302F93Ed732C88a8F1999dCe8e841E14EC", // Gabriel: DATA_AGENT_XMTP_ADDRESS
-    ];
-    const groupMembers = [creatorAddress, otherAddress, targetAddress, ...agentAddresses];
-
-    // Create the XMTP group conversation
-    const xmtpChat = await createGroupChat(groupTitle, groupImage, groupMembers);
-
     // Create the chat in Galadriel
     // TODO: Move from group-chat to nextjs
     const response = await fetch("http://localhost:3001/group-chats", {
@@ -51,7 +37,7 @@ export async function POST(request: Request) {
         privateInfo,
         groupTitle,
         groupImage,
-        groupId: xmtpChat.id,
+        // groupId: xmtpChat.id,
       }),
     });
 
@@ -75,7 +61,7 @@ export async function POST(request: Request) {
       groupTitle,
       groupImage,
       isCompleted: false,
-      groupId: xmtpChat.id,
+      groupId: groupChatData.groupId,
     };
     simulations.push(newSimulation);
 
