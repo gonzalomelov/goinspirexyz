df00fd18fffb1bdd5610878b08990d8fe3a0ba37
Gonzalo Melo <gonzalomelov@gmail.com>
2024-09-06 08:44:40 -0300

    chore: add missing contract

---
+++ packages/hardhat/deploy/03_deploy_open_ai_chat_gpt.ts
@@ -0,0 +1,29 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const deployAll: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const oracleAddress = process.env.ORACLE_ADDRESS;
+  if (!oracleAddress) {
+    throw new Error("ORACLE_ADDRESS is not set in the environment variables");
+  }
+
+  console.log("Deploying contracts with the account:", deployer);
+
+  // Deploy OpenAiChatGpt
+  console.log("Deploying OpenAiChatGpt...");
+  const OpenAiChatGpt = await deploy("OpenAiChatGpt", {
+    from: deployer,
+    args: [oracleAddress],
+    log: true,
+  });
+
+  console.log(`OpenAiChatGpt deployed to ${OpenAiChatGpt.address}`);
+};
+
+export default deployAll;
+
+deployAll.tags = ["OpenAiChatGpt"];
